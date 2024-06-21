d4774b844a8f12689c56fab3a6fb5a4cd8ef51b9
aninhasalesp <aninha.sales58@hotmail.com>
2024-06-21 14:52:11 -0300

    - Improved and expanded the project documentation for better clarity and usability.

--- youtool/commands/base.py
+++ youtool/commands/base.py
@@ -5,17 +5,36 @@ from typing import List, Dict, Any
 
 class Command():
     """
-    classe para os comandos herdarem dela, obedecendo a estrutura
+    A base class for commands to inherit from, following a specific structure.
+    
+    Attributes:
+        name (str): The name of the command.
+        arguments (List[Dict[str, Any]]): A list of dictionaries, each representing an argument for the command.
     """
     name: str
     arguments: List[Dict[str, Any]]
 
     @classmethod
     def generate_parser(cls, subparsers: argparse._SubParsersAction):
+        """
+        Creates a parser for the command and adds it to the subparsers.
+
+        Args:
+            subparsers (argparse._SubParsersAction): The subparsers action to add the parser to.
+
+        Returns:
+            argparse.ArgumentParser: The parser for the command.
+        """
         return subparsers.add_parser(cls.name, help=cls.__doc__)
 
     @classmethod
     def parse_arguments(cls, subparsers: argparse._SubParsersAction) -> None:
+        """
+        Parses the arguments for the command and sets the command's execute method as the default function to call.
+
+        Args:
+            subparsers (argparse._SubParsersAction): The subparsers action to add the parser to.
+        """
         parser = cls.generate_parser(subparsers)
         for argument in cls.arguments:
             argument_copy = {**argument}
@@ -25,4 +44,11 @@ class Command():
 
     @classmethod
     def execute(cls, arguments: argparse.Namespace):
-        """método que vai executar o comando"""
+        """
+        Executes the command.
+
+        This method should be overridden by subclasses to define the command's behavior.
+
+        Args:
+            arguments (argparse.Namespace): The parsed arguments for the command.
+        """

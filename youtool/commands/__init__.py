595e7ed025df7e09cb1feee0ef0f7d1a466dce74
aninhasalesp <aninha.sales58@hotmail.com>
2024-06-25 14:47:42 -0300

    - Included ChannelInfo in the list of commands in COMMANDS.

--- youtool/commands/__init__.py
+++ youtool/commands/__init__.py
@@ -1,10 +1,12 @@
 from .base import Command
 from .channel_id import ChannelId
+from .channel_info import ChannelInfo
 
 COMMANDS = [
     ChannelId,
+    ChannelInfo,
 ]
 
 __all__ = [
-    "Command", "COMMANDS", "ChannelId",
+    "Command", "COMMANDS", "ChannelId", "ChannelInfo",
 ]

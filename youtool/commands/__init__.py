50d937f3e9f7761086fd3edc20710c25e43e33de
aninhasalesp <aninha.sales58@hotmail.com>
2024-06-30 19:29:28 -0300

    Add new command in list

--- youtool/commands/__init__.py
+++ youtool/commands/__init__.py
@@ -1,12 +1,14 @@
 from .base import Command
 from .channel_id import ChannelId
 from .channel_info import ChannelInfo
+from .video_info import VideoInfo
 
 COMMANDS = [
     ChannelId,
     ChannelInfo,
+    VideoInfo,
 ]
 
 __all__ = [
-    "Command", "COMMANDS", "ChannelId", "ChannelInfo",
+    "Command", "COMMANDS", "ChannelId", "ChannelInfo", "VideoInfo",
 ]

3061ca8aa2c31b0c9ead09ee112dde696fa0205f
aninhasalesp <aninha.sales58@hotmail.com>
2024-07-02 12:40:16 -0300

    Add update

--- tests/commands/test_base.py
+++ tests/commands/test_base.py
@@ -113,7 +113,7 @@ def test_data_from_csv_column_not_found(mock_csv_file):
             file_path = Path("tests/resources/csv_column_not_found.csv")
             with pytest.raises(Exception) as exc_info:
                 Command.data_from_csv(file_path, "NonExistentColumn")
-            assert "Column NonExistentColumn not found on tests/resources/csv_column_not_found.csv" in str(exc_info.value)
+            assert f"Column NonExistentColumn not found on {file_path}" in str(exc_info.value)
 
 
 @pytest.fixture

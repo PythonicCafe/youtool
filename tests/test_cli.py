cd0017c7bd2fe6a176c6393558d880172ebd1e57
aninhasalesp <aninha.sales58@hotmail.com>
2024-07-05 01:00:03 -0300

    - Add test for video_transcription command; - Add some necessary improvements in other files

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,6 @@
 import pytest
 
+from pathlib import Path
 from subprocess import run
 
 from youtool.commands import COMMANDS
@@ -12,9 +13,13 @@ from youtool.commands.base import Command
 )
 def test_missing_api_key(monkeypatch: pytest.MonkeyPatch, command: Command):
     monkeypatch.delenv('YOUTUBE_API_KEY', raising=False)
-    cli_path = "youtool/cli.py"
-    command = ["python", cli_path, command.name]
-    result = run(command, capture_output=True, text=True, check=False)
+    cli_path = Path("youtool") / "cli.py"
+    command_string = ["python", cli_path, command.name]
+    for arg in command.arguments:
+        if arg.get("required"):
+            command_string.append(arg.get("name"))
+            command_string.append("test_value")
+    result = run(command_string, capture_output=True, text=True, check=False)
 
     assert result.returncode == 2
     assert "YouTube API Key is required" in result.stderr

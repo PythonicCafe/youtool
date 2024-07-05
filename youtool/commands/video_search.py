0e02e776963354f88c732d08faf5f7ce6e9052ac
aninhasalesp <aninha.sales58@hotmail.com>
2024-07-04 21:32:42 -0300

    - Add test for video_search command; - Add updates to some test files; - created conftest file

--- youtool/commands/video_search.py
+++ youtool/commands/video_search.py
@@ -8,7 +8,9 @@ from .base import Command
 
 
 class VideoSearch(Command):
-    """Search video info from a list of IDs or URLs (or CSV filename with URLs/IDs inside), generate CSV output (simplified video dict schema or option to get full video info)
+    """
+    Search video info from a list of IDs or URLs (or CSV filename with URLs/IDs inside),
+    generate CSV output (simplified video dict schema or option to get full video info)
     """
     name = "video-search"
     arguments = [
@@ -16,7 +18,9 @@ class VideoSearch(Command):
         {"name": "--urls", "type": str, "help": "Video URLs", "nargs": "*"},
         {"name": "--input-file-path", "type": str, "help": "Input CSV file path with URLs/IDs"},
         {"name": "--output-file-path", "type": str, "help": "Output CSV file path"},
-        {"name": "--full-info", "type": bool, "help": "Option to get full video info", "default": False}
+        {"name": "--full-info", "type": bool, "help": "Option to get full video info", "default": False},
+        {"name": "--url-column-name", "type": str, "help": "URL column name on csv input files"},
+        {"name": "--id-column-name", "type": str, "help": "Channel ID column name on csv output files"}
     ]
 
     ID_COLUMN_NAME: str = "video_id"
@@ -27,23 +31,6 @@ class VideoSearch(Command):
     FULL_INFO_COLUMNS: List[str] = [
         "id", "title", "description", "published_at", "view_count", "like_count", "comment_count"
     ]
-
-    @staticmethod
-    def filter_fields(video_info: Dict, info_columns: Optional[List] = None) -> Dict:
-        """Filters the fields of a dictionary containing video information based on specified columns.
-
-        Args:
-            video_info (Dict): A dictionary containing video information.
-            info_columns (Optional[List], optional): A list specifying which fields to include in the filtered output. 
-            If None, returns the entire video_info dictionary. Defaults to None.
-
-        Returns:
-            A dictionary containing only the fields specified in info_columns (if provided) 
-            or the entire video_info dictionary if info_columns is None.
-        """
-        return {
-            field: value for field, value in video_info.items() if field in info_columns
-        } if info_columns else video_info
 
     @classmethod
     def execute(cls: Self, **kwargs) -> str:
@@ -68,21 +55,20 @@ class VideoSearch(Command):
         """
         ids = kwargs.get("ids", [])
         urls = kwargs.get("urls", [])
-        input_file_path = kwargs.get("input_file_path")
         output_file_path = kwargs.get("output_file_path")
         api_key = kwargs.get("api_key")
         full_info = kwargs.get("full_info", False)
 
+        url_column_name = kwargs.get("url_column_name", cls.URL_COLUMN_NAME)
+        id_column_name = kwargs.get("id_column_name", cls.ID_COLUMN_NAME)
+
         info_columns = VideoSearch.FULL_INFO_COLUMNS if full_info else VideoSearch.INFO_COLUMNS
 
-        if input_file_path:
-            with open(input_file_path, mode='r') as infile:
-                reader = csv.DictReader(infile)
-                for row in reader:
-                    if cls.ID_COLUMN_NAME in row and row[cls.ID_COLUMN_NAME]:
-                        ids.append(row[cls.ID_COLUMN_NAME])
-                    elif cls.URL_COLUMN_NAME in row and row[cls.URL_COLUMN_NAME]:
-                        urls.append(row[cls.URL_COLUMN_NAME])
+        if (input_file_path := kwargs.get("input_file_path")):
+            if (urls_from_csv := cls.data_from_csv(input_file_path, url_column_name)):
+                ids += [cls.video_id_from_url(url) for url in urls_from_csv]
+            if (ids_from_csv := cls.data_from_csv(input_file_path, id_column_name)):
+                ids += ids_from_csv
 
         if not ids and not urls:
             raise Exception("Either 'ids' or 'urls' must be provided for the video-search command")
@@ -95,7 +81,7 @@ class VideoSearch(Command):
         # Remove duplicated
         ids = list(set(ids))
         videos_infos = list(youtube.videos_infos([_id for _id in ids if _id]))
-                                     
+
         return cls.data_to_csv(
             data=[
                 VideoSearch.filter_fields(
